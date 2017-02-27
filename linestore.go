a3f203faa9ff0cef7ab06ee01d2b58e5200679cd
Sidney Kochman <sidney@kochman.org>
2017-02-27 13:13:04 -0500

    go fmt

--- linestore.go
+++ linestore.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"github.com/ugorji/go/codec"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
-    "time"
-    "bytes"
-    "io/ioutil"
-    "io"
-    "net/http"
-    "encoding/json"
-    "strings"
-    "github.com/ugorji/go/codec"
+	"strings"
+	"time"
 )
 
 type LSRecord struct {
-    Id string
-    Timestamp time.Time
-    Event string
-    Value string
+	Id        string
+	Timestamp time.Time
+	Event     string
+	Value     string
 }
 
 type LSFile struct {
@@ -26,7 +26,7 @@ type LSFile struct {
 }
 
 func (ls *LSFile) Write(data []byte) {
-    // seek to end of file
+	// seek to end of file
 	_, err := ls.f.Seek(0, 2)
 	if err != nil {
 		log.Fatal(err)
@@ -40,39 +40,39 @@ func (ls *LSFile) Write(data []byte) {
 }
 
 func (ls *LSFile) WriteRecord(record LSRecord) {
-    buf := &bytes.Buffer{}
+	buf := &bytes.Buffer{}
 
-    mh := &codec.MsgpackHandle{}
-    enc := codec.NewEncoder(buf, mh)
-    err := enc.Encode(record)
-    if err != nil {
-        log.Fatal(err)
-    }
+	mh := &codec.MsgpackHandle{}
+	enc := codec.NewEncoder(buf, mh)
+	err := enc.Encode(record)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    ls.Write(buf.Bytes())
+	ls.Write(buf.Bytes())
 }
 
 func (ls *LSFile) ReadRecords() (records []LSRecord) {
-    data := ls.read()
-    buf := bytes.NewBuffer(data)
+	data := ls.read()
+	buf := bytes.NewBuffer(data)
 
-    for {
-        mh := &codec.MsgpackHandle{}
-        enc := codec.NewDecoder(buf, mh)
-        record := &LSRecord{}
-        err := enc.Decode(record)
-        if err == io.EOF {
-            break
-        } else if err != nil {
-            log.Fatal(err)
-        }
-        records = append(records, *record)
-    }
-    return
+	for {
+		mh := &codec.MsgpackHandle{}
+		enc := codec.NewDecoder(buf, mh)
+		record := &LSRecord{}
+		err := enc.Decode(record)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		records = append(records, *record)
+	}
+	return
 }
 
 func (ls *LSFile) read() (data []byte) {
-    // seek to beginning of file
+	// seek to beginning of file
 	_, err := ls.f.Seek(0, 0)
 	if err != nil {
 		log.Fatal(err)
@@ -83,29 +83,29 @@ func (ls *LSFile) read() (data []byte) {
 		log.Fatal(err)
 	}
 
-    // chop off header
-    recordStart := bytes.Index(data, []byte("\n\n"))
-    data = data[recordStart+2:]
+	// chop off header
+	recordStart := bytes.Index(data, []byte("\n\n"))
+	data = data[recordStart+2:]
 
-    return
+	return
 }
 
 func (ls *LSFile) ReadRecordsForId(id string) (records []LSRecord) {
-    for _, record := range ls.ReadRecords() {
-        if record.Id == id {
-            records = append(records, record)
-        }
-    }
-    return
+	for _, record := range ls.ReadRecords() {
+		if record.Id == id {
+			records = append(records, record)
+		}
+	}
+	return
 }
 
 func (ls *LSFile) WriteHeader() {
-    var header string
+	var header string
 
-    header += "linestore-lsfile/v1\n"
-    header += "\n"
+	header += "linestore-lsfile/v1\n"
+	header += "\n"
 
-    ls.Write([]byte(header))
+	ls.Write([]byte(header))
 }
 
 func createFile() (ls *LSFile) {
@@ -116,7 +116,7 @@ func createFile() (ls *LSFile) {
 
 	ls = &LSFile{f: f}
 
-    ls.WriteHeader()
+	ls.WriteHeader()
 
 	return
 }
@@ -134,65 +134,64 @@ func openFile() (ls *LSFile) {
 // HTTP interface
 
 func httpReadRecords(w http.ResponseWriter, r *http.Request) {
-    buf := &bytes.Buffer{}
-    enc := json.NewEncoder(buf)
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
 
-    ls := openFile()
-    err := enc.Encode(ls.ReadRecords())
-    if err != nil {
-        log.Fatal(err)
-    }
+	ls := openFile()
+	err := enc.Encode(ls.ReadRecords())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    io.Copy(w, buf)
+	io.Copy(w, buf)
 }
 
 func httpReadRecordsForId(w http.ResponseWriter, r *http.Request) {
-    url := r.URL
-    path := strings.Split(url.Path, "/")
-    if len(path) == 5 {
-        httpCreateRecordForId(w, r)
-        return
-    }
-    id := path[2]
+	url := r.URL
+	path := strings.Split(url.Path, "/")
+	if len(path) == 5 {
+		httpCreateRecordForId(w, r)
+		return
+	}
+	id := path[2]
 
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
 
-    buf := &bytes.Buffer{}
-    enc := json.NewEncoder(buf)
+	ls := openFile()
+	err := enc.Encode(ls.ReadRecordsForId(id))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    ls := openFile()
-    err := enc.Encode(ls.ReadRecordsForId(id))
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    io.Copy(w, buf)
+	io.Copy(w, buf)
 }
 
 func httpCreateRecordForId(w http.ResponseWriter, r *http.Request) {
-    url := r.URL
-    path := strings.Split(url.Path, "/")
-    id := path[2]
-    event := path[3]
-    value := path[4]
+	url := r.URL
+	path := strings.Split(url.Path, "/")
+	id := path[2]
+	event := path[3]
+	value := path[4]
 
-    record := &LSRecord{
-        Id: id,
-        Timestamp: time.Now(),
-        Event: event,
-        Value: value,
-    }
+	record := &LSRecord{
+		Id:        id,
+		Timestamp: time.Now(),
+		Event:     event,
+		Value:     value,
+	}
 
-    ls := openFile()
-    ls.WriteRecord(*record)
+	ls := openFile()
+	ls.WriteRecord(*record)
 
-    buf := &bytes.Buffer{}
-    enc := json.NewEncoder(buf)
-    err := enc.Encode(record)
-    if err != nil {
-        log.Fatal(err)
-    }
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
+	err := enc.Encode(record)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    io.Copy(w, buf)
+	io.Copy(w, buf)
 }
 
 func main() {
@@ -201,20 +200,20 @@ func main() {
 
 	ls := createFile()
 
-    for _, val := range []string{"hello", "world", "these", "are", "records"} {
-        record := LSRecord{
-            Id: "example",
-            Timestamp: time.Now(),
-            Event: "event",
-            Value: val,
-        }
-        ls.WriteRecord(record)
-    }
+	for _, val := range []string{"hello", "world", "these", "are", "records"} {
+		record := LSRecord{
+			Id:        "example",
+			Timestamp: time.Now(),
+			Event:     "event",
+			Value:     val,
+		}
+		ls.WriteRecord(record)
+	}
 
-    numRecords := len(ls.ReadRecords())
-    log.Print("[INFO] " + strconv.Itoa(numRecords) + " records stored")
+	numRecords := len(ls.ReadRecords())
+	log.Print("[INFO] " + strconv.Itoa(numRecords) + " records stored")
 
-    http.HandleFunc("/records", httpReadRecords)
-    http.HandleFunc("/records/", httpReadRecordsForId)
-    log.Fatal(http.ListenAndServe("127.0.0.1:8009", nil))
+	http.HandleFunc("/records", httpReadRecords)
+	http.HandleFunc("/records/", httpReadRecordsForId)
+	log.Fatal(http.ListenAndServe("127.0.0.1:8009", nil))
 }
